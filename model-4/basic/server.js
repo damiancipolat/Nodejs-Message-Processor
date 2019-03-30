c368453b222c91a88cd7a981d6bd33e68b7c1e27
Damian Cipolat <damian.cipolat@gmail.com>
2019-03-30 01:47:35 -0300

    fix config file

--- model-4/basic/server.js
+++ model-4/basic/server.js
@@ -1,32 +1,55 @@
-const net = require('net');
-const port = 8081;
+const net  = require('net');
 const host = '127.0.0.1';
 
+//Get port from console.
+const port = ((process.argv.length==4)&&(process.argv[2]=='--port'))?process.argv[3]:8081;
+
 const server = net.createServer();
-server.listen(port, host, () => {
-    console.log('TCP Server is running on port ' + port + '.');
-});
+let  sockets = [];
 
-let sockets = [];
+//ON LISTEN.
+const onListen = () => {
 
-server.on('connection', function(sock) {
-    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-    sockets.push(sock);
+  console.log('TCP Server is running on port ' + port + '.');
 
-    sock.on('data', function(data) {
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        // Write the data back to all the connected, the client will receive it as data from the server
-        sockets.forEach(function(sock, index, array) {
-            sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
-        });
-    });
+}
 
-    // Add a 'close' event handler to this instance of socket
-    sock.on('close', function(data) {
-        let index = sockets.findIndex(function(o) {
-            return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
-        })
-        if (index !== -1) sockets.splice(index, 1);
-        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
-    });
-});
+//ON CONNECTION.
+const onConnection = (sock) => {
+
+  console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+
+  //Save the socket in the array.
+  sockets.push(sock);
+
+  sock.on('data', (data)=>{
+
+      console.log('DATA ' + sock.remoteAddress + ': ' + data);
+
+      // Write the data back to all the connected, the client will receive it as data from the server
+      sockets.forEach((sock, index, array)=>{
+          sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
+      });
+
+  });
+
+  // Add a 'close' event handler to this instance of socket
+  sock.on('close', (data)=>{
+
+      let index = sockets.findIndex((o)=>{
+          return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+      })
+
+      if (index !== -1) sockets.splice(index, 1);
+
+      console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+
+  });
+
+}
+
+//Start listen server.
+server.listen(port, host, onListen);
+
+//When a connection is received.
+server.on('connection', onConnection);
